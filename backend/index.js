b71727a6888194108efc7e707e518664194b3909
ragini954 <gokhrurags@gmail.com>
2023-01-27 11:37:37 +0530

    Creating Login Api-Lecture 3

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,39 @@
-const express = require('express')
-const connectToMongo=require('./db/config')
-const User=require('./db/User')
-const cors=require('cors')
-const app = express()
-const port = 5000
-connectToMongo()
+const express = require("express");
+const connectToMongo = require("./db/config");
+const User = require("./db/User");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+connectToMongo();
 
-app.use(cors())
-app.use(express.json())
-app.post('/register', async (req,res)=>{
-    let user=new User(req.body)
-    let result=await user.save()
-    res.send(result)
-})
+app.use(cors());
+app.use(express.json());
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject(); //at the time of save we can't use select to delete any field from response.
+  delete result.password;
+  res.send(result);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post("/login", async (req, res) => {
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      res.send(user);
+    }
+  } else {
+    res.send({ result: "No User found" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
-//get method is used when we need a page to load
-//post,put,patch,delete these methods are used while form submission,etc.
+//get method is used when we need a page to load and show some data //post,put,patch,delete these methods are used while form submission,etc.
 //response(res) is sent by the server(backend) on the request(req) by client(frontend)
